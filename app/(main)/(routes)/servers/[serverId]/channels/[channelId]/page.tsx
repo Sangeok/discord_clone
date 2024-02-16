661a93aa29a0d78de99e2a0795f1d7b00416cd3c
HamSangeok <hamsoo159@naver.com>
2024-02-17 00:06:11 +0900

    chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -38,11 +39,23 @@ const ChannelIdpage = async ({params}:ChannelIdpageProps) => {
 
 
     return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full ">
             <ChatHeader 
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+            <div className="flex-1">
+                Future Messages
+            </div>
+            <ChatInput 
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/message"
+                query ={{
+                    serverId : channel.serverId,
+                    channelId : channel.id
+                }}
             />
         </div>
     )
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ const MainLayout = async({
     children : React.ReactNode;
 }) => {
     return (
-        <div>
+        <div className="h-full">
             <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
                 <NavigationSidebar />
             </div>

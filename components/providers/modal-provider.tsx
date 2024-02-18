018148a438b640a72015661294a81616b8630c57
HamSangeok <hamsoo159@naver.com>
2024-02-18 11:01:07 +0900

    Message Attachment

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -11,6 +11,7 @@ import { Delete } from "lucide-react";
 import DeleteServerModal from "../modals/delete-server-modal";
 import DeleteChannelModal from "../modals/delete-channel-modal";
 import EditChannelModal from "../modals/edit-channel-modal";
+import MessageFileModal from "../modals/message-file-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -32,6 +33,7 @@ export const ModalProvider = () => {
             <DeleteServerModal />
             <DeleteChannelModal />
             <EditChannelModal />
+            <MessageFileModal />
         </div>
     )
 }
